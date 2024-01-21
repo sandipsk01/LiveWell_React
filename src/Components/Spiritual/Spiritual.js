32b9fc656ede93390964ba4c9cf8eb5922d92b43
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2024-01-22 01:16:19 +0530

    Spiritual Videos added

--- src/Components/Spiritual/Spiritual.js
+++ src/Components/Spiritual/Spiritual.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import spiritual from "./spirituality.jpg";
 import videos from "./video.json";
 function Spiritual() {
+  const [content, setContent] = useState("video");
+
+  const handleContent = (cont) => {
+    setContent(cont);
+  };
   return (
     <div className="bg-slate-200 flex flex-col items-center">
-      <div className="sm:w-[70vw] mb-2 sm:my-3  ">
-        <img className="border rounded-lg" src={spiritual} alt="spiritual" />
+      <div className=" sm:w-[70vw] mb-2 sm:my-3  ">
+        <img
+          className=" h-[45vh] sm:h-auto border sm:rounded-lg"
+          src={spiritual}
+          alt="spiritual"
+        />
 
-        <h5 className="my-2 text-2xl font-serif  text-gray-700">
+        <h5 className="my-2 text-[20px] font-serif  text-gray-700">
           Spirituality is a fundamental aspect of human experience, which can
           significantly benefit physical and mental health. Research has shown
           that spirituality can reduce stress and anxiety and improve coping
@@ -15,19 +24,41 @@ function Spiritual() {
         </h5>
       </div>
 
-    <h3 className="my-2 text-[30px] font-serif font-semibold text-gray-800">SPIRITUAL VIDEOS</h3>
-      <div className="flex flex-wrap justify-center">
-        {videos.map((video, index)=>{
-            return(
-                <iframe title={index} className="w-[300px] h-[200px] mx-6 my-4 rounded-lg" allowFullScreen={true} src={`https://www.youtube.com/embed/videoseries?list=${video.videoID}&rel=0`} type="video/mp4" />
-
-            )
-        })}
+      <div className="my-2 text-[30px] font-serif font-semibold ">
+        <button
+          className={content === "video" ? "text-gray-800" : "text-gray-500"}
+          onClick={() => {
+            handleContent("video");
+          }}
+        >
+          SPIRITUAL VIDEOS
+        </button>
+        |
+        <button
+          className={content === "book" ? "text-gray-800" : "text-gray-500"}
+          onClick={() => {
+            handleContent("book");
+          }}
+        >
+          SPIRITUAL BOOKS
+        </button>
       </div>
 
+      <div className="flex flex-wrap justify-center">
+        {videos.map((video, index) => {
+          return (
+            <iframe
+              title={index}
+              key={index}
+              className="w-[300px] h-[200px] mx-6 my-4 rounded-lg"
+              allowFullScreen={true}
+              src={`https://www.youtube.com/embed/videoseries?list=${video.videoID}&rel=0`}
+              type="video/mp4"
+            />
+          );
+        })}
+      </div>
     </div>
-
-
   );
 }
 

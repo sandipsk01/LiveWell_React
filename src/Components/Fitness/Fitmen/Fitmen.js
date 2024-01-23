25826e663896a65cc7ddf23a95a237a08737578e
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2024-01-23 18:55:51 +0530

    Fitness Plans

--- src/Components/Fitness/Fitmen/Fitmen.js
+++ src/Components/Fitness/Fitmen/Fitmen.js
@@ -11,9 +11,23 @@ export default function Fitmen() {
         />
 
         <h5 className="my-2 text-[20px] font-serif  text-gray-700">
-        Whether you’re looking to shed pounds or add bulk, here’s how to tailor your workout to help you reach your fitness goals.
+        Whether you're looking to shed pounds or add bulk, here's how to tailor your workout to help you reach your fitness goals.
         </h5>
       </div>
+      <div className="flex justify-around w-[80vw] space-x-2 space-y-1 sm:space-y-0 items-center flex-wrap p-1 mx-auto my-2 bg-gray-100 rounded-lg dark:bg-slate-900" role="group">
+            <button type="button" className="px-5 py-1.5 w-[70vw] sm:w-[150px] font-medium text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700 rounded-lg">
+                Home
+            </button>
+            <button type="button" className="px-5 py-1.5 w-[70vw] sm:w-[150px] font-medium text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900 rounded-lg">
+                Beginner
+            </button>
+            <button type="button" className="px-5 py-1.5 w-[70vw] sm:w-[150px] font-medium text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700 rounded-lg">
+                Intermediate
+            </button>
+            <button type="button" className="px-5 py-1.5 w-[70vw] sm:w-[150px] font-medium text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700 rounded-lg">
+                Advanced
+            </button>
+        </div>
     </div>
   )
 }

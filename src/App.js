3beae269ff8b5def35f2932cd3dc4fa49f5fd7f1
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2024-03-04 12:02:51 +0530

    logged in and logout completed temp

--- src/App.js
+++ src/App.js
@@ -24,13 +24,11 @@ function App() {
         <Navbar doc={[docId, setDocId]}/>
           <Routes>
             <Route path="/" element={docId===null?<Login setDocId={setDocId}/>:<Home />} />
-            <Route path="/register" element={<Register/>} />
-            <Route path="/home" element={<Home />}/>
-            <Route path="/calorie" element={<Calorie />}/>
-            <Route path="/spiritual" element={<Spiritual />}/>
-            <Route path="/men" element={<Fitmen />} />
-            <Route path="/women" element={<Fitwomen/>} />
-            <Route path="/yoga" element={<Yoga/>} />
+            <Route path="/calorie" element={docId===null?<Login setDocId={setDocId}/>:<Calorie />}/>
+            <Route path="/spiritual" element={docId===null?<Login setDocId={setDocId}/>:<Spiritual />}/>
+            <Route path="/men" element={docId===null?<Login setDocId={setDocId}/>:<Fitmen />} />
+            <Route path="/women" element={docId===null?<Login setDocId={setDocId}/>:<Fitwomen/>} />
+            <Route path="/yoga" element={docId===null?<Login setDocId={setDocId}/>:<Yoga/>} />
             <Route path="/register" element={<Register/>}/>
           </Routes>
         <Footer />
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -73,7 +73,7 @@ export default function Navbar({doc:[docId, setDocId]}) {
           >
             <li>
               <NavLink
-                to="/home"
+                to="/"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
                 aria-current="page"
               >
